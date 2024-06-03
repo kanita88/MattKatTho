af92630ad1476fc18139b6a8ad5468ca2932b3e5
Kanita88 <kanitalim88@gmail.com>
2024-06-03 12:27:21 +0200

    Initial Commit

---
+++ MattKatTho/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MattKatTho
+//
+//  Created by Apprenant 172 on 03/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MattKatTho/MattKatThoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MattKatThoApp.swift
+//  MattKatTho
+//
+//  Created by Apprenant 172 on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MattKatThoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
